1d9a5139c02b1105e36c4ee5931e6c94ff1ac22a
vojin111 <vojinc111@gmail.com>
2026-02-06 21:20:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taksi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aaaaaaa
+            Console.WriteLine("AAAAAAAA");
+
+
+        }
+    }
+}
